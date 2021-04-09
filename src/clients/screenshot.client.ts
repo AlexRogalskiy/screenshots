022eb7d3e01c7d4ad01c77eaacfa5d75f991e473
ADMSK\AVROGAL1 <avrogal1@mts.ru>
2021-04-09 22:58:29 +0300

    docs: updates on workflows
    Added information on documentation

--- src/clients/screenshot.client.ts
+++ src/clients/screenshot.client.ts
@@ -1,18 +1,10 @@
-import {
-    GeneralPageOptions,
-    ImageOptions,
-    PuppeteerPageOptions,
-    RequestOptions,
-    ResourceOptions,
-} from '../../typings/browser-types'
+import { PuppeteerPageOptions, RequestOptions } from '../../typings/browser-types'
 
-import { profile } from '../utils/profiles'
-import { mergeProps } from '../utils/commons'
 import { boxenLogs } from '../utils/loggers'
 import { serialize } from '../utils/serializers'
 import { createBrowserSession } from '../utils/sessions'
 
-const getSessionScreenshot = async (request: Required<RequestOptions>): Promise<Buffer | string | void> => {
+export async function screenshotRenderer(request: Required<RequestOptions>): Promise<Buffer | string | void> {
     boxenLogs(`>>> Generating screenshot with parameters: ${serialize(request)}`)
 
     const { routeOptions, imageOptions, resourceOptions, pageOptions } = request
@@ -31,17 +23,4 @@ const getSessionScreenshot = async (request: Required<RequestOptions>): Promise<
     } finally {
         await session.teardown()
     }
-}
-
-export async function screenshotRenderer(request: RequestOptions): Promise<Buffer | string | void> {
-    const { imageOptions, resourceOptions, pageOptions } = profile.screenshotOptions
-
-    const requestData: Required<RequestOptions> = {
-        routeOptions: request.routeOptions,
-        imageOptions: mergeProps<ImageOptions>(imageOptions, request.imageOptions),
-        resourceOptions: mergeProps<ResourceOptions>(resourceOptions, request.resourceOptions),
-        pageOptions: mergeProps<GeneralPageOptions>(pageOptions, request.pageOptions),
-    }
-
-    return await getSessionScreenshot(requestData)
 }
--- src/services/template.service.ts
+++ src/services/template.service.ts
@@ -1,12 +1,28 @@
-import { RequestOptions } from '../../typings/browser-types'
+import {
+    GeneralPageOptions,
+    ImageOptions,
+    RequestOptions,
+    ResourceOptions,
+} from '../../typings/browser-types'
 
 import * as screenshotClient from '../clients/screenshot.client'
 
-import { boxenLogs } from '../utils/loggers'
 import { serialize } from '../utils/serializers'
+import { mergeProps } from '../utils/commons'
+import { boxenLogs } from '../utils/loggers'
+import { profile } from '../utils/profiles'
 
 export async function templateRenderer(request: RequestOptions): Promise<Buffer | string | void> {
     boxenLogs(`>>> Generating screenshot with request parameters: ${serialize(request)}`)
 
-    return await screenshotClient.screenshotRenderer(request)
+    const { imageOptions, resourceOptions, pageOptions } = profile.screenshotOptions
+
+    const requestData: Required<RequestOptions> = {
+        routeOptions: request.routeOptions,
+        imageOptions: mergeProps<ImageOptions>(imageOptions, request.imageOptions),
+        resourceOptions: mergeProps<ResourceOptions>(resourceOptions, request.resourceOptions),
+        pageOptions: mergeProps<GeneralPageOptions>(pageOptions, request.pageOptions),
+    }
+
+    return await screenshotClient.screenshotRenderer(requestData)
 }
